d0f7c1feb892c3ef122271e4947e597b1405e391
anafletcher <153273057+anafletcher@users.noreply.github.com>
2025-10-24 12:57:36 +0100

    Added footer with link my portfolio page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import "./App.css";
 
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>City Weather Display</h1>
         <Weather />
+        <footer className="App-footer">
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://my-project-portfolio-afletcher.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Alicia Fletcher
+            </a>
+          </p>
+        </footer>
       </header>
     </div>
   );
 }
-
-export default App;
